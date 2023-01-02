e7d4e67cc4cd7b60a3f7e7c7a1cec911ad535323
fmuklis@gmail.com <fmuklis@gmail.com>
2023-01-02 22:15:07 +0700

    - Implement Api Broker to Post  Student Async - Added Student Model - Added Student Gender Enum

---
+++ BlazorApps/Brokers/API/ApiBroker.Student.cs
@@ -0,0 +1,12 @@
+﻿using BlazorApps.Models.Students;
+using System.Threading.Tasks;
+
+namespace BlazorApps.Brokers.API;
+
+public partial class ApiBroker
+{
+    private const string relativeUrl = "api/students";
+
+    public async ValueTask<Student> PostStudentAsync(Student student) =>
+        await PostAsync(relativeUrl, student);
+}
---
+++ BlazorApps/Brokers/API/IApiBroker.Student.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using BlazorApps.Models.Students;
+using System.Threading.Tasks;
+
+namespace BlazorApps.Brokers.API;
+
+public partial interface IApiBroker
+{
+    ValueTask<Student> PostStudentAsync(Student student);
+}
